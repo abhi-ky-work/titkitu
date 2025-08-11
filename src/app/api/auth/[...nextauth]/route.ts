c318bfe5dd2cda98c40ab47535d450c6ebe96a8e
abhi-ky-work <abhi.ky.work@gmail.com>
2025-08-11 22:19:22 +0100

    login feature added

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+const handler = NextAuth({
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: `${process.env.KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}`,
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
+    }),
+  ],
+  // secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) token.accessToken = account.access_token;
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST}
