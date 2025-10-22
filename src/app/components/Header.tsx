17d5ca711b5c497d14a0bf76ef93d4e090414f71
abhi-ky-work <abhi.ky.work@gmail.com>
2025-10-23 00:35:45 +0530

    [fix] : header fix with sticky and boders

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { Button } from "@/components/ui/button";
 import { LogIn, Search, Ticket } from "lucide-react";
-
+import { signIn } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Header() {
     const searchSubmitHandler = () =>{
@@ -10,10 +11,10 @@ export default function Header() {
 
   
     return (
-    <header className="bg-white shadow fixed top-0 left-0 right-0 z-50 glass-effect  w-full" >
-        <div className="w-10/12 border border-black rounded-[2px] flex items-center justify-between ml-auto mr-auto ">
+    <header className="bg-white shadow sticky top-0 left-0 right-0 z-50 w-full" >
+        <div className="w-10/12 flex items-center justify-between ml-auto mr-auto ">
             {/* haeder container */}
-            <div className="w-1/5 px-4 sm:px-6 lg:px-8 flex  items-center h-16 border border-black rounded-[2px]">
+            <div className="w-1/5 px-4 sm:px-6 lg:px-8 flex  items-center h-16">
                 <div > <Ticket className="" color="orange" /> </div>
                 <div className="text-2xl font-bold bg-gradient-to-br from-indigo-600 to-purple-600 bg-clip-text text-transparent ">TikiTu</div>
             </div>
@@ -25,20 +26,16 @@ export default function Header() {
                     <input type="text" placeholder="Search events, venues..." className="outline-none p-2 w-96" />
                 </form>
             </div>
-            <div className="w-1/5 flex items-center justify-between  border border-black rounded-[2px]">
+            <div className="w-1/5 flex items-center justify-between ">
 
-                <Button variant="ghost" className="m-4 hover:text-purple-600 hover:bg-purple-50">
+                <Button variant="ghost" onClick={()=> redirect('/')} className="m-4 hover:text-purple-600 hover:bg-purple-50">
                     Browse Events
                 </Button>
 
-                <Button variant="outline" className="m-4 border-purple-200 text-purple-600 hover:bg-purple-50"  >
+                <Button variant="outline" onClick={()=> signIn("keycloak",{ callbackUrl: "/dashboard" })} className="m-4 border-purple-200 text-purple-600 hover:bg-purple-50"  >
                     <LogIn  />
-                SignIn
+                    SignIn
                 </Button>
-                
-                {/* <Button  variant="outline" className="m-4" >
-                SignUp
-                </Button> */}
             </div>
             
         </div>
